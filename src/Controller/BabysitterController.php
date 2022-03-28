4b78246540f7aa05f3071c864b61d9c0c472520e
Grace <gracemusabe@gmail.com>
2022-03-28 16:53:20 +0200

    Updates in admin template

--- src/Controller/BabysitterController.php
+++ src/Controller/BabysitterController.php
@@ -17,6 +17,7 @@ class BabysitterController extends AbstractController
     #[Route('/', name: 'app_babysitter_index', methods: ['GET'])]
     public function index(BabysitterRepository $babysitterRepository): Response
     {
+
         return $this->render('babysitter/index.html.twig', [
             'babysitters' => $babysitterRepository->findAll(),
         ]);
@@ -25,6 +26,8 @@ class BabysitterController extends AbstractController
     #[Route('/new', name: 'app_babysitter_new', methods: ['GET', 'POST'])]
     public function new(Request $request, BabysitterRepository $babysitterRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        
         $babysitter = new Babysitter();
         $form = $this->createForm(Babysitter1Type::class, $babysitter);
         $form->handleRequest($request);
@@ -46,7 +49,8 @@ class BabysitterController extends AbstractController
 
                 $babysitter->setPicture($fileName);
             }
-            return $this->redirectToRoute('app_babysitter_index', [], Response::HTTP_SEE_OTHER);
+            // return $this->redirectToRoute('app_babysitter_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin');
         }
 
         return $this->renderForm('babysitter/new.html.twig', [
@@ -83,6 +87,8 @@ class BabysitterController extends AbstractController
     #[Route('/{id}', name: 'app_babysitter_delete', methods: ['POST'])]
     public function delete(Request $request, Babysitter $babysitter, BabysitterRepository $babysitterRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         if ($this->isCsrfTokenValid('delete'.$babysitter->getId(), $request->request->get('_token'))) {
             $babysitterRepository->remove($babysitter);
         }
